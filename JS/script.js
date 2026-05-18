b4b69d2923415872b552775a828797356a8dea70
bwit25dset-ms <bwit25d.set@ismt.edu.np>
2026-05-18 10:08:53 +0545

    up

---
+++ JS/script.js
@@ -0,0 +1,81 @@
+// // console
+// console.log("Shreshant Wagle!");
+// console.warn("Warning!");
+// console.error("Error!");
+
+// variable types
+// const x = 10;
+// console.log(x);
+
+// let y = 20;
+// console.log(y);
+
+// z = 30;
+// console.log(z);
+
+// let a = 10;
+// let b = "20";
+// console.log(typeof a);
+// console.log(typeof b);
+
+// console.log(a + b);
+
+// strings
+// numbers
+// boolean
+// null
+// undefined
+// symbol-symbol()
+// object
+// array
+// function
+// bigint -232-1 to 232-1
+
+// operators
+// arithmetic operators
+// assignment operators
+// comparison operators
+// logical operators
+
+// arithmetic operators
+a = 10;
+b = 20;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+console.log(a++);
+console.log(a--);
+console.log(--a);
+console.log(++a);
+console.log(a);
+
+// assignment operators
+let c = 10;
+console.log(c);
+console.log(c += 5);
+console.log(c -= 5);
+console.log(c *= 5);
+console.log(c /= 5);
+console.log(c %= 5);
+console.log(c **= 5);
+
+// relational operators
+let d = 10;
+console.log(c > d);
+console.log(c < d);
+console.log(c >= d);
+console.log(c <= d);
+console.log(c == d);
+console.log(c === d);
+console.log(c != d);
+console.log(c !== d);
+
+// conditional operators
+let e = 10;
+let f = 20;
+console.log(e > f ? "e is greater than f" : "f is greater than e");
+
+
